db927e367891b4d13247aa07cc86b402b7d0a725
CreditariaUC <91077674+CreditariaUC@users.noreply.github.com>
2024-12-10 17:52:49 -0600

    Update tailwind.config.js [BoltSync]

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,74 @@ export default {
   ],
   theme: {
     extend: {
-     
+      colors: {
+       
+        light: {
+          primary: {
+            50: '#f0f9ff',
+            100: '#e0f2fe',
+            200: '#bae6fd',
+            300: '#7dd3fc',
+            400: '#38bdf8',
+            500: '#4F46E5',
+            600: '#0284c7',
+            700: '#4F46E5',
+            800: '#075985',
+            900: '#0c4a6e',
+            950: '#082f49',
+          },
+          background: '#f8fafc',
+          
+          
+          border: '#e2e8f0',
+          input: '#e2e8f0',
+        },
+    
+        
+      }
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            background: '#f8fafc',
+            
+            primary: {
+              50: '#f0f9ff',
+              100: '#e0f2fe',
+              200: '#bae6fd',
+              300: '#7dd3fc',
+              400: '#38bdf8',
+              500: '#4F46E5',
+              600: '#0284c7',
+              700: '#4F46E5',
+              800: '#075985',
+              900: '#0c4a6e',
+              950: '#082f49',
+              DEFAULT: '#4F46E5',
+             
+            },
+            
+          },
+          layout: {
+            disabledOpacity: "0.3",
+            radius: {
+              small: "4px",
+              medium: "6px",
+              large: "8px"
+            },
+            borderWidth: {
+              small: "1px",
+              medium: "2px",
+              large: "3px"
+            }
+          }
+        },
+       
+      },
+    }),
+  ],
 };
